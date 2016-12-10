07500dbc6fdc47da86c434269e20d3db06052f81
Ryan Baxley <ryan.baxley1@gmail.com>
2016-12-10 18:38:15 -0500

    lib/league.js

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect;
 const espnff = require('../index.js');
 const rewire = require('rewire');
+const _ = require('lodash');
 
 describe("ESPN Fantasy Football", () => {
   describe("League object tests", () => {
@@ -44,6 +45,23 @@ describe("ESPN Fantasy Football", () => {
         return league.getLeagueName().then((result) => {
           throw new Error('Test should have failed!');
         }, () => {}); // Need to handle error otherwise mocha will fail
+      });
+    });
+
+    describe("Getting scoreboard data", () => {
+      it("Should get the current week's scores", () => {
+        let league = espnff.league(1829677);
+        return league.getWeek().then(res => {
+          expect(_.has(res[0], 'teamName')).to.equal(true);
+        });
+      });
+
+
+      it("Should get the current season's scores", () => {
+        let league = espnff.league(1829677);
+        return league.getCurrentSeason().then(res => {
+          expect(_.has(res[0][0], 'teamName')).to.equal(true);
+        });
       });
     });
   });
